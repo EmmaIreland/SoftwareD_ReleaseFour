d3091dbc032474b539fb59a6c520946fd3bccfa6
irela065 <irela065@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-04-17 01:46:52 +0000

    Added geb tests for course page.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseFour/TwentyFourEyes@9660 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- test/functional/CoursePageSpec.groovy
+++ test/functional/CoursePageSpec.groovy
@@ -3,18 +3,59 @@ import spock.lang.*
 import pages.*
 import pages.CourseShowPage
 import pages.CourseCreatePage
+import pages.CourseEditPage
+import pages.CourseListPage
 import pages.ProjectCreatePage
 
 class CoursePageSpec extends GebReportingSpec {
-    
+
+    def "home button on course list page should go to home page"() {
+        when:
+        to CourseListPage
+        homeButton.click()
+
+        then:
+        at HomePage
+    }
+
+    def "new course button on course list page should go to course create"() {
+        when:
+        to CourseListPage
+        newCourseButton.click()
+
+        then:
+        at CourseCreatePage
+    }
+
     def "course create should go to course show"() {
-	when:
-	to CourseCreatePage
+        when:
+        to CourseCreatePage
         courseAbbreviationBox.value("SA")
         courseNameBox.value("Some Name")
-	courseCreateButton.click()
-	
-	then:
-	at CourseShowPage
+        courseCreateButton.click()
+
+        then:
+        at CourseShowPage
+    }
+
+    def "new course button on course show page should go to course create"() {
+        when:
+        to CourseShowPage
+        newCourseButton.click()
+
+        then:
+        at CourseCreatePage
+    }
+    
+    def "course list button on course show page should go to course list"() {
+        when:
+        to CourseCreatePage
+        courseAbbreviationBox.value("SA")
+        courseNameBox.value("Some Name")
+        courseCreateButton.click()
+        courseListButton.click()
+
+        then:
+        at CourseListPage
     }
 }
