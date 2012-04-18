f03b05618e4906a175ea096b6bc7a6f686948e03
virat001 <virat001@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-04-18 01:56:58 +0000

    Fixed bug where survey show buttons didn't work.
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseFour/TwentyFourEyes@9684 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,32 +5,32 @@ import grails.util.GrailsUtil
 class BootStrap {
 
     def init = { servletContext ->
-        
+
         if (Person.count() == 0) { //GrailsUtil.environment != 'production') {
-			def failOnError = [failOnError: true]
-			def springString = 'Spring'			
-			
-            Person admin = new Person(name: 'Sid Anderson', password: 'shiboleet',
-				email: 'sid@anderson.net', isAdmin: true)
-            //Dropping this to a newline to make Sonar happy
-            admin.save(failOnError)
-            
-            Person nic = new Person(name: 'Nic McPhee', password: 'thomas', 
-				email: 'mcphee@morris.umn.edu').save(failOnError)
+            def failOnError = [failOnError: true]
+            def springString = 'Spring'
+
+            new Person(name: 'Sid Anderson',
+                       password: 'shiboleet',
+                       email: 'sid@anderson.net',
+                       isAdmin: true).save(failOnError)
+
+            Person nic = new Person(name: 'Nic McPhee',password: 'thomas',
+                    email: 'mcphee@morris.umn.edu').save(failOnError)
             Person kkLamberty = new Person(name: 'KK Lamberty', password: 'spencer',
-				email: 'lamberty@morris.umn.edu').save(failOnError)
-            
+                    email: 'lamberty@morris.umn.edu').save(failOnError)
+
             Course softwareDesign = new Course(abbreviation:'CSCI 3601', name:'Software Design',
-				 term: springString,year: 2012, owner: nic).save(failOnError)
+                    term: springString,year: 2012, owner: nic).save(failOnError)
             Course introToComputing = new Course(abbreviation:'CSCI 1001', name:'Intro to the Computing World',
-				 term: springString,year: 2013, owner: kkLamberty).save(failOnError)
-            
+                    term: springString,year: 2013, owner: kkLamberty).save(failOnError)
+
             def defaultPassword = 'password'
             def buildPerson = { person -> new Person(name: person[0] + ' ' + person[1],
-				password: defaultPassword, email: person[2]) }
+                password: defaultPassword, email: person[2]) }
             def enrollPerson = { person, course -> new Enrollment(person: person,
-				course: course).save(failOnError) }
-            
+                course: course).save(failOnError) }
+
             def softwareDesignPeople = []
             [
                 ['Phil', 'McGraw', 'phil@gmail.com'],
@@ -50,11 +50,11 @@ class BootStrap {
                 ['Annabel', 'Leef', 'annabel@poe.org'],
                 ['Phou', 'Lee', 'phou@lee.me'],
                 ['Scott', 'Steffes', 'scott@steffes.me']
-		
+
             ].each { person -> softwareDesignPeople.add(buildPerson(person)) }
             softwareDesignPeople.each { it.save(failOnError) }
             softwareDesignPeople.each { enrollPerson it, softwareDesign }
-            
+
             def introToComputingPeople = []
             [
                 ['James', 'Aronson', 'james@aronson.me'],
@@ -63,64 +63,64 @@ class BootStrap {
                 ['George', 'Bush', 'george@bush.com'],
                 ['Joevin', 'Einertyosin', 'joevin@einertyosin.org'],
                 ['Reed', 'Wallace', 'reed@wallace.me'],
-		['Adrian', 'Schiller', 'imsocool@seriously.nah'],
-		['Denard', 'Span', 'span@twins.com'],
-		['Jamie', 'Carroll', 'carroll@twins.com'],
-		['Joe','Mauer','mauer@twins.com'],
-		['Justin','Morneau','morneau@twins.com'],
-		['Josh','Willingham','willingham@twins.com'],
-		['Ryan','Doumit','dournit@twins.com'],
-		['Danny','Valencia','valencia@twins.com'],
-		['Christie','Parmelee','parmelee@twins.com'],
-		['Alexi','Casilla','casilla@twins.com']
-		
+                ['Adrian', 'Schiller', 'imsocool@seriously.nah'],
+                ['Denard', 'Span', 'span@twins.com'],
+                ['Jamie', 'Carroll', 'carroll@twins.com'],
+                ['Joe','Mauer','mauer@twins.com'],
+                ['Justin','Morneau','morneau@twins.com'],
+                ['Josh','Willingham','willingham@twins.com'],
+                ['Ryan','Doumit','dournit@twins.com'],
+                ['Danny','Valencia','valencia@twins.com'],
+                ['Christie','Parmelee','parmelee@twins.com'],
+                ['Alexi','Casilla','casilla@twins.com']
+
             ].each { person -> introToComputingPeople.add(buildPerson(person)) }
             introToComputingPeople.each { it.save(failOnError) }
             introToComputingPeople.each { enrollPerson it, introToComputing }
-            
+
             Project releaseOne = new Project(name: 'Release One', description: 'Students do cool things',
-				 course: softwareDesign, dueDate: new Date().next()).save(failOnError)
+                    course: softwareDesign, dueDate: new Date().next()).save(failOnError)
             Team teamAwesome = new Team(name: 'Team Awesome', project: releaseOne).save(failOnError)
             Team teamHumble = new Team(name: 'Team Humble', project: releaseOne).save(failOnError)
-            
+
             for (i in 0..3) {
                 new Membership(team: teamAwesome, member: softwareDesignPeople[i]).save(failOnError)
             }
             for (i in 6..10) {
                 new Membership(team: teamHumble, member: softwareDesignPeople[i]).save(failOnError)
             } // TODO: make these for loops w/o duplication
-                        
-	    ShortTextQuestion whatStoriesFinished = new ShortTextQuestion(prompt:'What stories did you complete?')
-            whatStoriesFinished.save(failOnError)
-	    MultipleChoiceQuestion teamGrade = new MultipleChoiceQuestion(prompt:'What grade would you give your group?',
-				 choices:['A', 'B', 'C', 'D', 'F']).save(failOnError)
-	    LongTextQuestion describeExperience = new LongTextQuestion(prompt:'Describe your group experience:')
-            describeExperience.save(failOnError)
-		
-           
+
+            def surveyReleaseOneQuestions = [
+                new ShortTextQuestion(prompt:'What stories did you complete?'),
+                new MultipleChoiceQuestion(prompt:'What grade would you give your group?',
+                    choices:['A', 'B', 'C', 'D', 'F']),
+                new LongTextQuestion(prompt:'Describe your group experience:')
+            ]
+
+
             def scootsSurveyQuestions = [
                 new LongTextQuestion(prompt: 'So, tell me \'bout yerself.'),
                 new CheckboxQuestion(prompt: 'Are you a boy or a girl?', choices: ['Boy', 'Girl', 'Other']),
                 new LongTextQuestion(prompt: 'And how d\'you feel \'bout that?'),
                 new ShortTextQuestion(prompt: 'If you were an animal, what animal would you be?'),
                 new MultipleChoiceQuestion(prompt: 'What\'s your favorite range in the visible light spectrum?',
-                    choices: ['380-450 (violet)', '450-475 (blue)', '476-495 (cyan)', '495-570 (green)',
-						'570-590 (yellow)', '590-620 (orange)', '620-750 (red)', '760+ (infrared)']),
+                choices: ['380-450 (violet)', '450-475 (blue)', '476-495 (cyan)', '495-570 (green)',
+                    '570-590 (yellow)', '590-620 (orange)', '620-750 (red)', '760+ (infrared)']),
                 new ShortTextQuestion(prompt: 'What is your favorite vowel-less word?'),
                 new LongTextQuestion(prompt: 'Tell me a story.'),
                 new CheckboxQuestion(prompt: 'Do you love me?', choices: ['Yes', 'More than yes (wink, wink)'])
             ]
-			
-			Survey surveyReleaseOne = new Survey(title: 'Survey for Release One', dueDate: new Date().next(),
-				questions: [whatStoriesFinished, teamGrade, describeExperience], project: releaseOne)
-			Survey scootsSurvey = new Survey(title: 'Scoot\'s Finest Survey', dueDate: new Date().next(),
-				questions: scootsSurveyQuestions, project: releaseOne)
-			
-			surveyReleaseOne.save(failOnError)
-			scootsSurvey.save(failOnError)
+
+            Survey surveyReleaseOne = new Survey(title: 'Survey for Release One', dueDate: new Date().next(),
+                    questions: surveyReleaseOneQuestions, project: releaseOne)
+            Survey scootsSurvey = new Survey(title: 'Scoot\'s Finest Survey', dueDate: new Date().next(),
+                    questions: scootsSurveyQuestions, project: releaseOne)
+
+            surveyReleaseOne.save(failOnError)
+            scootsSurvey.save(failOnError)
         }
     }
-    
+
     def destroy = {
     }
 }
--- grails-app/controllers/survey/SurveyController.groovy
+++ grails-app/controllers/survey/SurveyController.groovy
@@ -5,247 +5,247 @@ import grails.converters.JSON
 
 class SurveyController {
 
-	static post = 'POST'
-	def listString = 'list'
-	def editString = 'edit'
-	def createString = 'create'
-	def showString = 'show'
-	def flush = [flush: true]
-	def defaultNotFoundMessage = 'default.not.found.message'
-	def failOnError = [failOnError: true]
+    static post = 'POST'
+    def listString = 'list'
+    def editString = 'edit'
+    def createString = 'create'
+    def showString = 'show'
+    def flush = [flush: true]
+    def defaultNotFoundMessage = 'default.not.found.message'
+    def failOnError = [failOnError: true]
 
-	static allowedMethods = [save: post, update: post, delete: post, preview: post]
+    static allowedMethods = [save: post, update: post, delete: post, preview: post]
 
-	def index = {
-		redirect(action: listString, params: params)
-	}
+    def index = {
+        redirect(action: listString, params: params)
+    }
 
-	def list = {
-		params.max = Math.min(params.max ? params.int('max') : 10, 100)
-		[surveyInstanceList: Survey.list(params), surveyInstanceTotal: Survey.count()]
-	}
+    def list = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [surveyInstanceList: Survey.list(params), surveyInstanceTotal: Survey.count()]
+    }
 
-	def create = {
-		def surveyInstance = new Survey()
-		surveyInstance.properties = params
-		return [surveyInstance: surveyInstance]
-	}
+    def create = {
+        def surveyInstance = new Survey()
+        surveyInstance.properties = params
+        return [surveyInstance: surveyInstance]
+    }
 
-	def save = {
-            def surveyInstance = new Survey(params)
-            if (params.existingSurvey != 'new') {
-                def oldSurvey = Survey.get(params.existingSurvey)
-                surveyInstance.questions = []
-                
-                oldSurvey.questions.each { question ->
-                    surveyInstance.questions.add(question.copyQuestion())
-                }
-                
+    def save = {
+        def surveyInstance = new Survey(params)
+        if (params.existingSurvey != 'new') {
+            def oldSurvey = Survey.get(params.existingSurvey)
+            surveyInstance.questions = []
+
+            oldSurvey.questions.each { question ->
+                surveyInstance.questions.add(question.copyQuestion())
             }
-            
-		if (surveyInstance.save(flush)) {
-			flash.message = makeMessage('default.created.message', surveyInstance.title)
-			redirect(action: showString, id: surveyInstance.id)
-		}
-		else {
-			render(view: createString, model: [surveyInstance: surveyInstance])
-		}
-	}
+        }
 
-	private removeEmptyElements(def input) {
-		def list = input as List
-		list.retainAll{
-			it.length() > 0
-		}
-		return list
-	}
+        if (surveyInstance.save(flush)) {
+            flash.message = makeMessage('default.created.message', surveyInstance.title)
+            redirect(action: showString, id: surveyInstance.id)
+        }
+        else {
+            render(view: createString, model: [surveyInstance: surveyInstance])
+        }
+    }
 
-	def addQuestion = {
-		def surveyInstance = Survey.get(params.surveyid)
-		def surveyType = params.type
-		def questionInstance
+    private removeEmptyElements(def input) {
+        def list = input as List
+        list.retainAll{
+            it.length() > 0
+        }
+        return list
+    }
 
-		switch(surveyType){
-			case 'existing':
-				questionInstance = Question.get(params.questionid).copyQuestion()
-				break
-			case 'checkbox':
-				def choices = removeEmptyElements(params.cbChoices)
-				questionInstance = new CheckboxQuestion(prompt: params.cbPrompt,
-					 choices: choices).save(failOnError)
-				break
-			case 'multipleChoice':
-				def choices = removeEmptyElements(params.mcChoices)
-				questionInstance = new MultipleChoiceQuestion(prompt: params.mcPrompt,
-					 choices: choices).save(failOnError)
-				break
-			case 'shortResponse':
-				questionInstance = new ShortTextQuestion(prompt: params.stPrompt).save(failOnError)
-				break
-			case 'longResponse':
-				questionInstance = new LongTextQuestion(prompt: params.ltPrompt).save(failOnError)
-				break
-		}
-		surveyInstance.addToQuestions(questionInstance)
-		surveyInstance.save(failOnError)
-		render questionInstance as JSON
-	}
+    def addQuestion = {
+        def surveyInstance = Survey.get(params.surveyid)
+        def surveyType = params.type
+        def questionInstance
 
-	def show = {
-		def surveyInstance = Survey.get(params.id)
-		def existingQuestions = Question.findAll()
-		if (surveyInstance) {
-			[surveyInstance: surveyInstance, existingQuestions: existingQuestions]
-		}
-		else {
-			flash.message = makeMessage(defaultNotFoundMessage, params.id)
-			redirect(action: listString)
-		}
-	}
+        switch(surveyType){
+            case 'existing':
+                questionInstance = Question.get(params.questionid).copyQuestion()
+                break
+            case 'checkbox':
+                def choices = removeEmptyElements(params.cbChoices)
+                questionInstance = new CheckboxQuestion(prompt: params.cbPrompt,
+                        choices: choices).save(failOnError)
+                break
+            case 'multipleChoice':
+                def choices = removeEmptyElements(params.mcChoices)
+                questionInstance = new MultipleChoiceQuestion(prompt: params.mcPrompt,
+                        choices: choices).save(failOnError)
+                break
+            case 'shortResponse':
+                questionInstance = new ShortTextQuestion(prompt: params.stPrompt).save(failOnError)
+                break
+            case 'longResponse':
+                questionInstance = new LongTextQuestion(prompt: params.ltPrompt).save(failOnError)
+                break
+        }
+        surveyInstance.addToQuestions(questionInstance)
+        surveyInstance.save(failOnError)
+        render questionInstance as JSON
+    }
 
-	def preview = {
-		def surveyInstance = Survey.get(params.id)
-		if (surveyInstance) {
-			[surveyInstance: surveyInstance]
-		}
-		else {
-			flash.message = makeMessage(defaultNotFoundMessage, params.id)
-			redirect(action: listString)
-		}
-	}
+    def show = {
+        def surveyInstance = Survey.get(params.id)
+        def existingQuestions = Question.findAll()
+        if (surveyInstance) {
+            [surveyInstance: surveyInstance, existingQuestions: existingQuestions]
+        }
+        else {
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
+        }
+    }
 
-	def submit = {
-		def surveyInstance = Survey.get(params.id)
-		def personInstance = Person.get(params.personid)
-		if (!surveyInstance || !personInstance) {
-			flash.message = makeMessage(defaultNotFoundMessage, params.id)
-			redirect(action: listString)
-			return
-		}
-		def questions = surveyInstance.questions
+    def preview = {
+        def surveyInstance = Survey.get(params.id)
+        if (surveyInstance) {
+            [surveyInstance: surveyInstance]
+        }
+        else {
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
+        }
+    }
 
-
-		questions.each { question ->
-			def serverResponse = params[question.id.toString()]
-			if (serverResponse == null) {
-				createAnswer(question, personInstance, [])
-			} else {
-				createAnswer(question, personInstance, serverResponse)
-			}
-		}
-		redirect(controller: 'person', action: showString, id:personInstance.id)
-	}
-
-	private createAnswer(question, person, serverResponse) {
-		def answer
-		switch(question.templateName) {
-			case 'Long':
-			case 'Short':
-				answer = new TextAnswer(response: serverResponse,
-					 person: person, question: question).save(failOnError)
-				break
-			case 'MultipleChoice':
-				answer = new MultipleChoiceAnswer(responseIndex: serverResponse,
-					 person: person, question: question).save(failOnError)
-				break
-			case 'Checkbox':
-				def responseMap = [:]
-				if (serverResponse instanceof String) {
-					serverResponse = [serverResponse]
-				} else {
-					serverResponse = serverResponse as List
-				}
-
-				question.choices.eachWithIndex { choice, index ->
+    def submit = {
+        def surveyInstance = Survey.get(params.id)
+        def personInstance = Person.get(params.personid)
+        if (!surveyInstance || !personInstance) {
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
+            return
+        }
+        def questions = surveyInstance.questions
 
 
-					responseMap[index] = serverResponse.contains(index)
-				}
-				answer = new CheckboxAnswer(responses: responseMap, person: person, question: question)
-				answer.save(failOnError)
-				break
-			default:
-				break
-		}
-	}
+        questions.each { question ->
+            def serverResponse = params[question.id.toString()]
+            if (serverResponse == null) {
+                createAnswer(question, personInstance, [])
+            } else {
+                createAnswer(question, personInstance, serverResponse)
+            }
+        }
+        redirect(controller: 'person', action: showString, id:personInstance.id)
+    }
+
+    private createAnswer(question, person, serverResponse) {
+        def answer
+        switch(question.templateName) {
+            case 'Long':
+            case 'Short':
+                answer = new TextAnswer(response: serverResponse,
+                person: person, question: question).save(failOnError)
+                break
+            case 'MultipleChoice':
+                answer = new MultipleChoiceAnswer(responseIndex: serverResponse,
+                person: person, question: question).save(failOnError)
+                break
+            case 'Checkbox':
+                def responseMap = [:]
+                if (serverResponse instanceof String) {
+                    serverResponse = [serverResponse]
+                } else {
+                    serverResponse = serverResponse as List
+                }
+
+                question.choices.eachWithIndex { choice, index ->
 
 
-	def take = {
-		def surveyInstance = Survey.get(params.id)
-		def personInstance = Person.get(session['user'])
-		if (!surveyInstance || !personInstance) {
-			flash.message = makeMessage(defaultNotFoundMessage, params.id)
-			redirect(action: listString)
-		}
-		else {
-			[surveyInstance: surveyInstance, personInstance: personInstance]
-		}
-	}
+                    responseMap[index] = serverResponse.contains(index)
+                }
+                answer = new CheckboxAnswer(responses: responseMap, person: person, question: question)
+                answer.save(failOnError)
+                break
+            default:
+                break
+        }
+    }
 
-	def edit = {
-		def surveyInstance = Survey.get(params.id)
-		if (surveyInstance) {
-			return [surveyInstance: surveyInstance]
-		}
-		else {
-			flash.message = makeMessage(defaultNotFoundMessage, params.id)
-			redirect(action: listString)
-		}
-	}
 
-	def update = {
-		def surveyInstance = Survey.get(params.id)
-		if (surveyInstance) {
-			if (params.version) {
-				def version = params.version.toLong()
-				if (surveyInstance.version > version) {
+    def take = {
+        def surveyInstance = Survey.get(params.id)
+        def personInstance = Person.get(params.personid)
+        if (!surveyInstance || !personInstance) {
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
+        }
+        else {
+            [surveyInstance: surveyInstance, personInstance: personInstance]
+        }
+    }
 
-					surveyInstance.errors.rejectValue('version', 'default.optimistic.locking.failure',
-						 [makeMessage('survey.label', surveyInstance.id)]
-					as Object[], 'Another user has updated this Survey while you were editing')
-					render(view: editString, model: [surveyInstance: surveyInstance])
-					return
-				}
-			}
-			surveyInstance.properties = params
-			if (!surveyInstance.hasErrors() && surveyInstance.save(flush)) {
-				flash.message = makeMessage('default.updated.message', surveyInstance.title)
-				redirect(action: showString, id: surveyInstance.id)
-			}
-			else {
-				render(view: editString, model: [surveyInstance: surveyInstance])
-			}
-		}
-		else {
-			flash.message = makeMessage(defaultNotFoundMessage, params.id)
-			redirect(action: listString)
-		}
-	}
+    def edit = {
+        def surveyInstance = Survey.get(params.id)
+        if (surveyInstance) {
+            return [surveyInstance: surveyInstance]
+        }
+        else {
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
+        }
+    }
 
-	def delete = {
-		def surveyInstance = Survey.get(params.id)
-		if (surveyInstance) {
-			try {
-				surveyInstance.delete(flush)
-				flash.message = makeMessage('default.deleted.message', surveyInstance.title)
-				redirect(action: listString)
-			}
-			catch (org.springframework.dao.DataIntegrityViolationException e) {
-				flash.message = makeMessage('default.not.deleted.message', params.id)
-				redirect(action: showString, id: params.id)
-			}
-		}
-		else {
-			flash.message = makeMessage(defaultNotFoundMessage, params.id)
-			redirect(action: listString)
-		}
-	}
+    def update = {
+        def surveyInstance = Survey.get(params.id)
+        if (surveyInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (surveyInstance.version > version) {
 
-	private makeMessage(code, surveyId) {
-		return "${message(code: code, args: [surveyLabel(), surveyId])}"
-	}
+                    surveyInstance.errors.rejectValue('version', 'default.optimistic.locking.failure',
+                            [
+                                makeMessage('survey.label', surveyInstance.id)]
+                            as Object[], 'Another user has updated this Survey while you were editing')
+                    render(view: editString, model: [surveyInstance: surveyInstance])
+                    return
+                }
+            }
+            surveyInstance.properties = params
+            if (!surveyInstance.hasErrors() && surveyInstance.save(flush)) {
+                flash.message = makeMessage('default.updated.message', surveyInstance.title)
+                redirect(action: showString, id: surveyInstance.id)
+            }
+            else {
+                render(view: editString, model: [surveyInstance: surveyInstance])
+            }
+        }
+        else {
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
+        }
+    }
 
-	private surveyLabel() {
-		message(code: 'survey.label', default: 'Survey')
-	}
+    def delete = {
+        def surveyInstance = Survey.get(params.id)
+        if (surveyInstance) {
+            try {
+                surveyInstance.delete(flush)
+                flash.message = makeMessage('default.deleted.message', surveyInstance.title)
+                redirect(action: listString)
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = makeMessage('default.not.deleted.message', params.id)
+                redirect(action: showString, id: params.id)
+            }
+        }
+        else {
+            flash.message = makeMessage(defaultNotFoundMessage, params.id)
+            redirect(action: listString)
+        }
+    }
+
+    private makeMessage(code, surveyId) {
+        return "${message(code: code, args: [surveyLabel(), surveyId])}"
+    }
+
+    private surveyLabel() {
+        message(code: 'survey.label', default: 'Survey')
+    }
 }
