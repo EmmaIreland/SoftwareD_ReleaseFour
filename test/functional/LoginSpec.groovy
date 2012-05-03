5e8c20b84ece9a4f3e97d208741ef59ed0b12196
virat001 <virat001@e5a9bd0e-96e4-4e5d-b9da-31dc9101860d>
2012-05-03 00:34:54 +0000

    Fixed Tests
    git-svn-id: http://cscirepo.morris.umn.edu/svn/csci-3601/Spring12/ReleaseFour/TwentyFourEyes@10019 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- test/functional/LoginSpec.groovy
+++ test/functional/LoginSpec.groovy
@@ -17,40 +17,39 @@ class LoginSpec extends GebReportingSpec {
 	at LoginPage
     }
     
-// commented out  for failing bootstrap for video
-//    def "logging in with correct data goes home"() {
-//        when:
-//        to LoginPage
-//        loginEmailField.value('mcphee@morris.umn.edu')
-//        loginPasswordField.value('thomas')
-//        loginButton.click()
-//        
-//        then:
-//        at HomePage
-//    }
+    def "logging in with correct data goes home"() {
+        when:
+        to LoginPage
+        loginEmailField.value('sid@anderson.net')
+        loginPasswordField.value('shiboleet')
+        loginButton.click()
+        
+        then:
+        at HomePage
+    }
     
     def "logging in with incorrect data goes back to LoginPage"() {
         when:
         to LoginPage
-        loginEmailField.value('mcphee@morris.umn.edu')
+        loginEmailField.value('sid@anderson.net')
         loginPasswordField.value('foo')
         loginButton.click()
         
         then:
         at LoginPage
     }
-//    
-//    def "logging out goes to LoginPage"() {
-//        when:
-//        to LoginPage
-//        loginEmailField.value('mcphee@morris.umn.edu')
-//        loginPasswordField.value('thomas')
-//        loginButton.click()
-//        to HomePage
-//        logoutButton.click()
-//        
-//        then:
-//        at LoginPage
-//    }
-//    
+    
+    def "logging out goes to LoginPage"() {
+        when:
+        to LoginPage
+        loginEmailField.value('sid@anderson.net')
+        loginPasswordField.value('shiboleet')
+        loginButton.click()
+        to HomePage
+        logoutButton.click()
+        
+        then:
+        at LoginPage
+    }
+    
 }
